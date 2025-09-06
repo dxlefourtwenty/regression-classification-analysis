478d8a480c193c951950ea33e1996065f2ec941b
Dale Peligro <daledoesminecraft2013@gmail.com>
2025-09-06 16:04:07 -0700

    added a second model for comparison

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
 
 # get the same results each time
 np.random.seed(0)
@@ -31,33 +32,33 @@ if False:
     print("Sample non-toxic comment:", comments_train.iloc[17])
 
 
-# train the model and evaluate performance on data provided
+# train the LR model and evaluate performance on data provided
 classifier = LogisticRegression(max_iter=2000)
 classifier.fit(X_train, y_train)
 score = classifier.score(X_test, y_test)
 
-# function classifies any string
-def classify(string, investigate=False):
+# function classifies any string using Logistic Regression
+def classifyLogReg(string, investigate=False):
     prediction = classifier.predict(vectorizer.transform([string]))[0]
     if prediction == 0:
         print("NOT TOXIC:", string)
     else:
         print("TOXIC:", string)
 
-# test model with example strings (comments)
-if True:
-    print("\nTesting with typical comments on social media posts:\n")
-    example_toxic = "This post is dumb"
-    example_toxic_2 = "Your outfit sucks bruh"
-    example_non_toxic = "I love your hair"
-    example_non_toxic_2 = "Your car is cool dude"
-    classify(example_toxic)
-    classify(example_non_toxic)
-    classify(example_non_toxic_2)
-    classify(example_toxic_2)
+# test LR model with example strings (comments)
+example_toxic = "This post is dumb"
+example_toxic_2 = "Your outfit sucks bruh"
+example_non_toxic = "I love your hair"
+example_non_toxic_2 = "Your car is cool dude"
+if False:
+    print("\n(LR Model) Testing with typical comments on social media posts:\n")
+    classifyLogReg(example_toxic)
+    classifyLogReg(example_non_toxic)
+    classifyLogReg(example_non_toxic_2)
+    classifyLogReg(example_toxic_2)
 
 
-# understanding the model with coefficients
+# understanding the models with coefficients
 if False:
     print("\nTop coefficients within the dataset:\n")
     coefficients = pd.DataFrame({"word" : sorted(list(vectorizer.
@@ -73,15 +74,45 @@ if False:
     print(top_pairs)
 
 
-# the bias of the model
+# the bias of the LR model
+bias_one = "I have a muslim friend"
+bias_two = "I have a christian friend"
+bias_three = "I have a white friend"
+bias_four = "I have a black friend"
+if False:
+    print("\n(LR Model) Testing the model's potential bias:\n")
+    classifyLogReg(bias_one)
+    classifyLogReg(bias_two)
+    classifyLogReg(bias_three)
+    classifyLogReg(bias_four)
+
+
+# train model #2 Linear SVC
+svm = LinearSVC()
+svm.fit(X_train, y_train)
+
+# function classifies any string with SVC
+def classifyLinSVC(string, investigate=False):
+    prediction = svm.predict(vectorizer.transform([string]))[0]
+    if prediction == 0:
+        print("NOT TOXIC:", string)
+    else:
+        print("TOXIC:", string)
+
+# test LSVC Model with example strings
 if True:
-    print("\nTesting the model's potential bias:\n")
-    comment_one = "I have a muslim friend"
-    comment_two = "I have a christian friend"
-    comment_three = "I have a white friend"
-    comment_four = "I have a black friend"
-    classify(comment_one)
-    classify(comment_two)
-    classify(comment_three)
-    classify(comment_four)
+    print("\n(LSVC Model) Testing with typical comments on social media posts:\n")
+    classifyLinSVC(example_non_toxic)
+    classifyLinSVC(example_toxic)
+    classifyLinSVC(example_non_toxic_2)
+    classifyLinSVC(example_toxic_2)
+
+# test LSVC Model bias
+if True:
+    print("\n(LSVC Model) Testing the model's potential bias:\n")
+    classifyLinSVC(bias_one)
+    classifyLinSVC(bias_two)
+    classifyLinSVC(bias_three)
+    classifyLinSVC(bias_four)
+
 
